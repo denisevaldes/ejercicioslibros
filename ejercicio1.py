a9e35715ab25a8c637f7848aa9d9e294ef38bd7d
denisevaldes <cadelige@gmail.com>
2021-08-27 23:49:01 -0400

    update

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,4 @@
 #write a program that uses input to prompt a user for their name and then welcomes them
 
 name = input('por favor, introduzca su nombre:\n')
-print('BHola',name )
+print('Hola',name )
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,3 +1,6 @@
 #write a program which prompts the user for a celsius temperature, convert the 
 #temperature to fahrenheit, and print out the converted temperature
 
+celsius = float(input('ingrese la temperatura en celsius:'))
+farenheit = (celsius * 9/5) + 32
+print('esta es la temperatura expresada en farenheit: ',farenheit)
